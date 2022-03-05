126bdc850e01e4e042b1d6654ee5e57f26cf4c32
PoojaSolankar <poojasolankar09gmail.com>
2022-03-05 19:00:44 +0530

    Get RollDice Number

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -7,18 +7,24 @@ public class SnakeandLadder {
 	public static void main(String[] args) {
 		SnakeNLadder s = new SnakeNLadder();
 		s.startGame();
-		
+		s.rollDice();
 	}
-
 }
-
 class SnakeNLadder
 {
+	
+	public void rollDice()
+	{
+		Random r = new Random();
+		int n=r.nextInt(7);
+		System.out.println("rollDice is :"+n);
+	}
 	
 	public void startGame()
 	{
 		int player1 = 0;
 		int currentplayerposition = 0;
-		System.out.println(player1+"......."+currentplayerposition);
+		 System.out.println("player :" +player1);
+	     System.out.println("currentplayerposition is :"+currentplayerposition);	
 	}
 }
