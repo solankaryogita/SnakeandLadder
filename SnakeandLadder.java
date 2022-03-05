065944fe67baf435c98073842823daa051c75110
PoojaSolankar <poojasolankar09gmail.com>
2022-03-05 18:45:38 +0530

    Game starting position

---
+++ SnakeandLadder.java
@@ -0,0 +1,24 @@
+package com.Bridgelabz;
+
+import java.util.Random;
+
+public class SnakeandLadder {
+
+	public static void main(String[] args) {
+		SnakeNLadder s = new SnakeNLadder();
+		s.startGame();
+		
+	}
+
+}
+
+class SnakeNLadder
+{
+	
+	public void startGame()
+	{
+		int player1 = 0;
+		int currentplayerposition = 0;
+		System.out.println(player1+"   "+currentplayerposition);
+	}
+}
