a7ea231b8e4403db8931a770e104c5c464e2e6ec
PoojaSolankar <poojasolankar09gmail.com>
2022-03-09 22:42:41 +0530

    Play the game

--- SnakeandLadder.java
+++ SnakeandLadder.java
@@ -1,6 +1,7 @@
 package com.Bridgelabz;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class SnakeandLadder {
 
@@ -12,19 +13,70 @@ public class SnakeandLadder {
 }
 class SnakeNLadder
 {
-	
-	public void rollDice()
+	final static int WINPOINT = 100;
+	public int rollDice()
 	{
+		int n = 0;
 		Random r = new Random();
-		int n=r.nextInt(7);
-		System.out.println("rollDice is :"+n);
+		n=r.nextInt(7);
+		return (n == 0 ? 1:n);
 	}
-	
+	public String snake() {
+		return "Snake";
+	}
+	public String ladder() {
+		return "Ladder";
+	}
 	public void startGame()
 	{
-		int player1 = 0;
-		int currentplayerposition = 0;
-		 System.out.println("player :" +player1);
-	     System.out.println("currentplayerposition is :"+currentplayerposition);	
+		int player1 =0, player2=0;
+		int currentPlayer=-1;
+		Scanner s = new Scanner(System.in);
+		String str;
+		int diceValue =0;
+		do
+		{
+			System.out.println(currentPlayer==-1?"\n\nFIRST PLAYER TURN":"\n\nSECOND PLAYER TURN");
+			System.out.println("Press r to roll Dice");
+			str = s.next();
+			diceValue = rollDice();
+			
+			
+			if(currentPlayer == -1)
+			{
+				player1 = calculatePlayerValue(player1,diceValue);
+				System.out.println("First Player :: " + player1);
+				//System.out.println("Second Player :: " + player2);
+				System.out.println("------------------");
+				if(isWin(player1))
+				{
+					System.out.println("First player wins");
+					return;
+				}
+			}
+			currentPlayer= -currentPlayer;	
+		}   while("r".equals(str));
+	}
+	public int calculatePlayerValue(int player, int diceValue)
+	{
+		player = player + diceValue;
+		if(null!=snake())
+		{
+			System.out.println("swallowed by snake");
+			player = player - diceValue; 
+		}
+		if(null!=ladder())
+		{
+			System.out.println("climb up the ladder");
+			player = player + diceValue; 
+		}
+		return player;
+	}
+	public boolean isWin(int player)
+	{
+		return WINPOINT == player;
 	}
 }
+	
+
+	
